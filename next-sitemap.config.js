38fa682a61425831c301961c4ae21ef13e5a33b4
Frame24 <hks9@yandex.ru>
2024-11-04 16:04:57 +0500

    Сделал генерацию sitemap и robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  exclude: ['/page'], // Указываем путь к странице, которую хотим исключить
+  siteUrl: 'https://popnailscz.netlify.app', // Замените на URL вашего сайта
+  generateRobotsTxt: true, // Создаст файл robots.txt
+  changefreq: 'weekly', // Частота обновления карты
+  priority: 0.7, // Приоритет страниц
+  sitemapSize: 5000,
+};
