30404435acdf345f92c57ef41cd3a2b53f40cc75
Sergei Khilimaniuk <145698293+Ingarent@users.noreply.github.com>
2025-09-21 17:12:09 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
-// pages/_app.tsx
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Google Ads Global Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16811680394"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16811680394');
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
